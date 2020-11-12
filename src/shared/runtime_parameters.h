51a610fa24aab6b6762d953d0f13da2a4ac27925
Glenn <glenntoews@outlook.com>
2020-11-12 22:54:44 +0100

    Fixed wrong filename in description

--- src/shared/runtime_parameters.h
+++ src/shared/runtime_parameters.h
@@ -5,7 +5,7 @@
  */
 
 /**
- * @file parameters.h
+ * @file runtime_parameters.h
  * @author Glenn Toews
  * @date 12 Nov 2020
  * @brief This file defines parameters which are generated during runtime
